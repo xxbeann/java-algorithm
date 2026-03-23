81789d94ea754641df17060b6fbd3575df44d1bf
Bean <yjb4945@gmail.com>
2026-03-23 16:03:37 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        answer = n;
+        for (int i = 0 ; i < t; i++) {
+            answer = answer * 2;
+        }
+        return answer;
+    }
+}
